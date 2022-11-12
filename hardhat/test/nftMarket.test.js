1ae541ff539cecf07fd7693eea682243f5d76fb4
PankajC-Tft <pankajc@applause.stream>
2022-11-12 22:15:09 +0530

    added test cases for token transfer

--- hardhat/test/nftMarket.test.js
+++ hardhat/test/nftMarket.test.js
@@ -4,7 +4,8 @@ const { ethers } = require("hardhat");
 let _contract = null;
 let _nftPrice = ethers.utils.parseEther("0.3").toString();
 let _listingPrice = ethers.utils.parseEther("0.025").toString();
-
+let ownerAddress;
+let address1;
 
 describe('Box', function () {
     before(async () => {
@@ -15,11 +16,10 @@ describe('Box', function () {
 
     describe("Mint token", () => {
         const tokenURI = "https://test.com";
-        let ownerAddress;
-
         before(async () => {
             const [owner, addr1, addr2] = await ethers.getSigners();
             ownerAddress = owner.address;
+            address1 = addr1.address;
             await _contract.mintToken(tokenURI, _nftPrice, {
                 from: ownerAddress,
                 value: _listingPrice
@@ -57,6 +57,70 @@ describe('Box', function () {
             expect(nftItem.price.toString()).to.be.equal(_nftPrice, "Nft price is not correct");
             expect(nftItem.creator).to.be.equal(ownerAddress, "Creator is not account[0]");
             expect(nftItem.isListed).to.be.true;
+        })
+    })
+
+    describe("Buy NFT", () => {
+        before(async () => {
+            const secondAddressSigner = await ethers.getSigner(address1)
+            await _contract.connect(secondAddressSigner).buyNft(1, {
+                from: address1,
+                value: _nftPrice
+            })
+        })
+
+        it("should unlist the item", async () => {
+            const listedItem = await _contract.getNftItem(1);
+            expect(listedItem.isListed).not.to.be.true;
+        })
+
+        it("should decrease listed items count", async () => {
+            const listedItemsCount = await _contract.listedItemsCount();
+            expect(listedItemsCount.toNumber()).to.be.equal(0, "Count has not been decrement");
+        })
+
+        it("should change the owner", async () => {
+            const currentOwner = await _contract.ownerOf(1);
+            expect(currentOwner).to.be.equal(address1);
+        })
+    })
+
+    describe("Token transfers", () => {
+        const tokenURI = "https://test-json-2.com";
+        before(async () => {
+            await _contract.mintToken(tokenURI, _nftPrice, {
+                from: ownerAddress,
+                value: _listingPrice
+            })
+        })
+
+        it("should have two NFTs created", async () => {
+            const totalSupply = await _contract.totalSupply();
+            expect(totalSupply.toNumber()).to.be.equal(2,"Total supply of token is not correct");
+        })
+
+        it("should be able to retreive nft by index", async () => {
+            const nftId1 = await _contract.tokenByIndex(0);
+            const nftId2 = await _contract.tokenByIndex(1);
+
+            expect(nftId1.toNumber()).to.be.equal(1,"Nft id is wrong");
+            expect(nftId2.toNumber()).to.be.equal(2,"Nft id is wrong");
+        })
+
+        it("should have one listed NFT", async () => {
+            const allNfts = await _contract.getAllNftsOnSale();
+            expect(allNfts[0].tokenId.toNumber()).to.be.equal(2,"Nft has a wrong id");
+        })
+
+        it("account[1] should have one owned NFT", async () => {
+            const secondAddressSigner = await ethers.getSigner(address1)
+            const ownedNfts = await _contract.connect(secondAddressSigner).getOwnedNfts({ from: address1 });
+            expect(ownedNfts[0].tokenId.toNumber()).to.be.equal(1,"Nft id is wrong");
+        })
+
+        it("account[0] should have one owned NFT", async () => {
+            const ownedNfts = await _contract.getOwnedNfts({ from: ownerAddress });
+            expect(ownedNfts[0].tokenId.toNumber()).to.be.equal(2,"Nft id is wrong");
         })
     })
 })
