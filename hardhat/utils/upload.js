afae780b11b1bbda8b7633e7b47364592f0ef111
arorashivam <arorashivam@protonmail.com>
2022-11-13 23:58:10 +0530

    Add JSON token URI support

--- hardhat/utils/upload.js
+++ hardhat/utils/upload.js
@@ -1,40 +1,63 @@
-const pinataSDK = require("@pinata/sdk");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const FormData = require("form-data");
 
-const key = process.env.PINATA_API_KEY;
-const secret = process.env.PINATA_API_SECRET;
 const JWT = process.env.PINATA_JWT;
 
 async function storeImage(imagesFilePath) {
   const fullpath = path.resolve(imagesFilePath);
 
-  try {
-    var data = new FormData();
-    data.append("file", fs.createReadStream(fullpath));
-    data.append("pinataOptions", '{"cidVersion": 1}');
-    data.append(
-      "pinataMetadata",
-      '{"name": "MyFile", "keyvalues": {"company": "Pinata"}}'
-    );
+  var data = new FormData();
+  data.append("file", fs.createReadStream(fullpath));
+  data.append("pinataOptions", '{"cidVersion": 1}');
+  data.append(
+    "pinataMetadata",
+    '{"name": "MyFile", "keyvalues": {"company": "Pinata"}}'
+  );
 
-    var config = {
-      method: "post",
-      url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-      headers: {
-        Authorization: `Bearer ${JWT}`,
-        ...data.getHeaders(),
+  var config = {
+    method: "post",
+    url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+    headers: {
+      Authorization: `Bearer ${JWT}`,
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
+
+  let res = await axios(config);
+
+  var tokenUriMetadata = JSON.stringify({
+    pinataOptions: {
+      cidVersion: 1,
+    },
+    pinataMetadata: {
+      name: "IpfsNFT",
+    },
+    pinataContent: {
+      name: "IpfsNFT",
+      keyvalues: {
+        image: `ipfs://${res.data.IpfsHash}`,
       },
-      data: data,
-    };
+    },
+  });
 
-    const res = await axios(config);
-    console.log(res.data);
+  var config = {
+    method: "post",
+    url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${JWT}`,
+    },
+    data: tokenUriMetadata,
+  };
 
-    return res.data.IpfsHash;
-  } catch {}
+  res = await axios(config);
+
+  console.log(tokenUriMetadata);
+
+  return res.data.IpfsHash;
 }
 
 module.exports = {
