ac9a2e787e12092436ba6129ce768d5f6fbba98b
atulsharmaTFT <sharma.atul@tftus.com>
2022-12-16 23:02:33 +0530

    Footer and Nav

---
+++ tft-emp-nft/pages/Components/MainListing.js
@@ -0,0 +1,16 @@
+import CardView from "./cardView"
+
+const MainListing = () =>{
+    return(
+<>
+<div className="px-28 py-8 flex flex-row bg-gradient-to-r from-[#240b36] to-[#240b36]">
+<CardView/>
+<CardView/>
+<CardView/>
+<CardView/>
+</div>
+</>
+    )
+}
+
+export default MainListing
--- tft-emp-nft/pages/_app.js
+++ tft-emp-nft/pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+import MainFooter from './Components/Mainfooter'
+import TopNavBar from './Components/TopNavBar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+<TopNavBar/>
+<Component {...pageProps} />
+<MainFooter/>
+</>
+  ) 
 }
