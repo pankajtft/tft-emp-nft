3d8aafd84254560a56bb51f084fc0c93d03f3272
atulsharmaTFT <sharma.atul@tftus.com>
2022-12-19 23:06:34 +0530

    dependencies for web3

--- tft-emp-nft/pages/Components/Hook-form/index.js
+++ tft-emp-nft/pages/Components/Hook-form/index.js
@@ -1,6 +1,5 @@
- export { default as RHFTextField } from "./RHFTextField";
+ export { default as RHFTextField } from "./RHFTextfield";
 export { default as RHDatepicker } from "./RHDatepicker";
 export {default as RHSelect} from "./RHSelect"
 export { default as RHRadioButton } from "./RHTRadioButton";
 export { default as FormProvider } from "./FormProvider";
-
