393734e259159aa06e99ce68b92dde694edec8a6
kumar-abhijeet-tft <kumar.abhijeet@tftus.com>
2022-12-20 19:42:20 +0530

    detail emp

--- tft-emp-nft/pages/Components/Hook-form/index.js
+++ tft-emp-nft/pages/Components/Hook-form/index.js
@@ -1,6 +1,7 @@
- export { default as RHFTextField } from "./RHFTextField";
+ export { default as RHFTextField } from "./RHFTextfield";
 export { default as RHDatepicker } from "./RHDatepicker";
 export {default as RHSelect} from "./RHSelect"
 export { default as RHRadioButton } from "./RHTRadioButton";
 export { default as FormProvider } from "./FormProvider";
+export { default as detailPageEmp } from "./detailPageEmp";
 
