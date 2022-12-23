533d5444dcd04840ccf21b504e9817e7b951ddd3
arorashivam <arora.shivam@tftus.com>
2022-12-23 17:28:37 +0530

    add contract call

--- client/pages/Components/ConnectButton.js
+++ client/pages/Components/ConnectButton.js
@@ -1,18 +1,19 @@
 import { Web3Context } from "../Context/web3Context";
-import React,{ useEffect } from "react";
+import React, { useEffect } from "react";
 const ConnectButton = () => {
   const { connect, hasMetamask, isConnected } = React.useContext(Web3Context);
-useEffect(() => {
-  const connectWalletOnPageLoad = async () => {
-    if (localStorage?.getItem("isWalletConnected") === "true") {
-      try {
-        await connect();
-      } catch (ex) {
-        console.log(ex);
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem("isWalletConnected") === "true") {
+        try {
+          await connect();
+        } catch (ex) {
+          console.log(ex);
+        }
       }
-    }}
-  connectWalletOnPageLoad();
-}, [isConnected]);
+    };
+    connectWalletOnPageLoad();
+  }, [isConnected]);
   return (
     <button
       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium px-4 mx-2"
---
+++ client/pages/Context/web3Context.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import { contractAddresses, abi } from "../contract-constants";
+import Web3 from "web3";
+import { ethers } from "ethers";
+
+const defaultValue = {};
+const Web3Context = React.createContext(defaultValue);
+const { Provider, Consumer } = Web3Context;
+
+const chainIdToNetwork = {
+  1: "Mainnet",
+  5: "Goerli",
+  31337: "Hardhat",
+};
+
+const Web3Provider = ({ children }) => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
+  const [contractToken, setContractToken] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [chainId, setChainID] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [address, setAddress] = useState(undefined);
+  const [network, setNetwork] = useState(undefined);
+  const [provider, setProvider] = useState(undefined);
+  const [nonce, setNonce] = useState(undefined);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setHasMetamask(true);
+      window.ethereum.on("chainChanged", (e) => {
+        setChainID(parseInt(e));
+      });
+      window.ethereum.on("accountsChanged", function (accounts) {
+        setAddress(accounts[0]);
+      });
+    }
+  }, []);
+  async function setValues() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    setSigner(provider.getSigner());
+    setChainID(window.ethereum.networkVersion);
+    setAddress(await signer.getAddress());
+    setNetwork(chainIdToNetwork[chainId]);
+    setIsConnected(true);
+    localStorage.setItem("isWalletConnected", true);
+
+    const address = contractAddresses["EMS"][chainId][0];
+
+    const NFTMarketContract = new ethers.Contract(address, abi, signer);
+    setContract(NFTMarketContract);
+  }
+
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await ethereum.request({ method: "eth_requestAccounts" });
+        await setValues();
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setIsConnected(false);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      setProvider(undefined);
+      setSigner(undefined);
+      setChainID(undefined);
+      setAddress(undefined);
+      setNetwork(undefined);
+      setIsConnected(undefined);
+      setContract(undefined);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  const addEmployee = () => {
+    console.log("addEmployee");
+  };
+  const editEmployeeDetails = () => {
+    console.log("Edit Employee");
+  };
+  const burnNft = () => {
+    console.log("burning NFT");
+  };
+  console.log(isConnected, "has");
+  return (
+    <Provider
+      value={{
+        connect,
+        disconnect,
+        isConnected,
+        contractToken,
+        chainId,
+        contract,
+        address,
+        network,
+        provider,
+        hasMetamask,
+        nonce,
+        isAuthenticated,
+      }}
+    >
+      {children}
+    </Provider>
+  );
+};
+
+export { Web3Context, Web3Provider };
