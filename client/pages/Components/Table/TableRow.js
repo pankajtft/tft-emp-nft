9444f752b39e180a9f81aaf48701cedc92b6e26e
arorashivam <arora.shivam@tftus.com>
2022-12-23 17:30:06 +0530

    add function to mint NFT

--- client/pages/Components/Table/TableRow.js
+++ client/pages/Components/Table/TableRow.js
@@ -1,11 +1,25 @@
 import { ButtonGroupIcon } from "../GroupButton";
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import moment from "moment";
+import { Web3Context } from "../../Context/web3Context";
 export const TableRow = ({ data }) => {
+  const { contract } = useContext(Web3Context);
+
   const Minted = [{ isMinted: false }];
+
   useEffect(() => {
     styleForMinted();
   }, [Minted?.isMinted]);
+
+  const mintNFTonETH = () => {
+    const tx = await contract
+      .mintEmployeeNFT
+      //params
+      ()
+    tx = tx.wait(1);
+    console.log(tx);
+  };
+
   function styleForMinted() {
     if (!Minted[0]?.isMinted) {
       return (
@@ -16,15 +30,13 @@ export const TableRow = ({ data }) => {
     } else {
       return (
         <>
-        {/* <p className="text-xs text-red-700 font-extrabold uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          {/* <p className="text-xs text-red-700 font-extrabold uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           Not Minted
         </p> */}
-        <button
-        className="uppercase text-xs bg-green-700 hover:bg-white text-white font-semibold hover:text-green-700 px-6 mx-4 border border-green-700 hover:border-green-700 rounded"
-      >
-        Mint NFT
-      </button>
-      </>
+          <button className="uppercase text-xs bg-green-700 hover:bg-white text-white font-semibold hover:text-green-700 px-6 mx-4 border border-green-700 hover:border-green-700 rounded">
+            Mint NFT
+          </button>
+        </>
       );
     }
   }
@@ -38,12 +50,16 @@ export const TableRow = ({ data }) => {
           lalit
           {data?._employeeName}
         </td> */}
-        <td class="py-4 px-6 text-center capitalize text-sm">{data?._employeeName}</td>
+        <td class="py-4 px-6 text-center capitalize text-sm">
+          {data?._employeeName}
+        </td>
         <td class="py-4 px-6 text-center capitalize text-sm">{data?._empId}</td>
         <td class="py-4 px-6 text-center text-sm">{data?.email}</td>
         <td class="py-4 px-6 text-center text-sm">
           {data?.skills?.map((item) => {
-            return <p className="text-left item-center text-sm capitalize">{item}</p>;
+            return (
+              <p className="text-left item-center text-sm capitalize">{item}</p>
+            );
           })}
         </td>
         <td class="py-4 px-6 text-center text-sm">
@@ -60,10 +76,20 @@ export const TableRow = ({ data }) => {
           {data?.projects &&
             moment(data?.projects?.[0]?.project_end_date).format("DD/MM/YYYY")}
         </td>
-        <td class="py-4 px-6 text-center text-sm">{data?.projects?.[0]?.team_size}</td>
-        <td class="py-2 px-6 text-center text-sm">{data?.projects && styleForMinted()}</td>
+        <td class="py-4 px-6 text-center text-sm">
+          {data?.projects?.[0]?.team_size}
+        </td>
+        <td class="py-2 px-6 text-center text-sm">
+          {data?.projects && styleForMinted()}
+        </td>
         <td class="py-4 px-6 w-10 ">
-          {<ButtonGroupIcon isEdit={true} isDelete={true} disabled={!!!data?.projects}/>}   
+          {
+            <ButtonGroupIcon
+              isEdit={true}
+              isDelete={true}
+              disabled={!!!data?.projects}
+            />
+          }
         </td>
       </tr>
     </>
