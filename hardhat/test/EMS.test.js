00443542486de9481fc2b1fa66db4aa164664853
Anurag Pathak <101109228+pathakanu@users.noreply.github.com>
2022-12-28 00:50:08 +0530

    updated test

--- hardhat/test/EMS.test.js
+++ hardhat/test/EMS.test.js
@@ -3,6 +3,7 @@ const { expect, assert } = require("chai");
 const eventemitter2 = require("chai-eventemitter2");
 chai.use(eventemitter2());
 const { ethers } = require("hardhat");
+const ch = require("@nomicfoundation/hardhat-chai-matchers");
 
 describe("NFT MARKETPLACE ", () => {
   let player1, player2, deployer, ems, emsContract;
@@ -14,7 +15,7 @@ describe("NFT MARKETPLACE ", () => {
     player2 = accounts[2];
     await deployments.fixture(["ems"]); // Deploys modules with the tags given
     emsContract = await ethers.getContract("EMS");
-    ems = emsContract.connect(player1);
+    ems = emsContract.connect(deployer);
   });
 
   describe("Deploying", function () {
@@ -26,13 +27,13 @@ describe("NFT MARKETPLACE ", () => {
   describe("Minting", function () {
     it("should mint nft contract", async () => {
       expect(
-        await ems.mintEmployeeNFT("ShivamArora", 17, "Project1", 100000, 20000)
+        await ems.mintEmployeeNFT("AnuragPathak", 17, "anu@test.com", "react,solidity", 7, "Project1", "100000", "20000")
       ).to.emit(ems, "NFTMinted");
     });
 
     it("should update nft contract", async () => {
-      await ems.mintEmployeeNFT("ShivamArora", 17, "Project1", 100000, 20000);
-      expect(await ems.updateEmployeeNFT(0, "Project2", 100000, 20000)).to.emit(
+      await ems.mintEmployeeNFT("AnuragPathak", 17, "anu@test.com", "react,solidity", 7, "Project1", "100000", "20000");
+      expect(await ems.updateEmployeeNFT(0, "hardhat,solidity", 4, "Project2", "1100000", "220000")).to.emit(
         ems,
         "NFTChanged"
       );
