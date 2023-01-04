247da0d36d7752503bfae0a0d919b094a9a9509a
Anurag Pathak <101109228+pathakanu@users.noreply.github.com>
2023-01-04 16:29:42 +0530

    Updated Contract

--- hardhat/contracts/EMS.sol
+++ hardhat/contracts/EMS.sol
@@ -14,24 +14,23 @@ import "hardhat/console.sol";
  */
  contract EMS is IERC721Receiver, ReentrancyGuard, Ownable {
     EmployeeNFT internal NFT;
-
+    
     struct Employee {
         bytes32 empDetails;
-        bytes32 projDetails;
+        bytes32 skillHash;
+        uint8 _currentProject;
+        bytes32[] projDetails;
     }
 
     mapping(uint8 => Employee) employees;
+    
 
     event NFTMinted(
-        uint16 _empId,
+        uint32 _empId,
         string employeeName,
         string email,
-        string skills,
-        uint16 team_size,
-        string _projectName,
-        string startTime,
-        string endTime,
-        uint8 tokenId
+        uint8 tokenId,
+        bytes32 _empHash
     );
 
     event NFTChanged(
@@ -47,54 +46,99 @@ import "hardhat/console.sol";
         NFT = new EmployeeNFT();
     }
 
+    //Mint new Employee
     function mintEmployeeNFT(
         string memory _employeeName,
-        uint16 _empId,
-        string memory email,
-        string memory _skills,
-        uint16 team_size,
-        string memory _projectName,
-        string memory startTime, //UnixTime
-        string memory endTime //UnixTime
-    ) public nonReentrant {
-        bytes32 empHash = keccak256(abi.encode(_employeeName, _empId, email));
-        bytes32 projectHash = keccak256(
-            abi.encode(_projectName, startTime, endTime, _skills, team_size)
-        );
-        bytes32 uriHash = keccak256(abi.encode(empHash, projectHash));
-        string memory uri = string(abi.encodePacked(uriHash));
-        uint8 tokenId = NFT.safeMint(uri);
+        uint32 _empId,
+        string memory email
+    ) public nonReentrant onlyOwner{
 
-        employees[tokenId] = Employee(empHash, projectHash);
+        Employee memory emp;
+        bytes32 empHash = keccak256(abi.encode(_employeeName, _empId, email));
+        string memory uri = string(abi.encodePacked(empHash));
+        uint8 tokenId = NFT.safeMint(uri);
+        emp.empDetails = empHash;
+        employees[tokenId] = emp;
 
         emit NFTMinted(
             _empId,
             _employeeName,
             email,
-            _skills,
-            team_size,
-            _projectName,
-            startTime,
-            endTime,
-            tokenId
+            tokenId,
+            empHash
         );
 
     }
 
-    function updateEmployeeNFT(
+    //Update Skills
+    function skillUpdate(uint8 tokenId, string memory _skills) public onlyOwner{
+        bytes32 _skillHash = keccak256(abi.encode(_skills));
+        employees[tokenId].skillHash = _skillHash;
+    } 
+
+
+    //Add new Project
+    function AddProject(
+        uint8 tokenId,
+        string memory _skills,
+        uint16 team_size,
+        string memory _projectName,
+        string memory startTime, //UnixTime
+        string memory endTime, //UnixTime
+        uint8 projectNumber
+    ) external nonReentrant onlyOwner{
+        
+        bytes32 projectHash = keccak256(
+            abi.encode(_projectName, startTime, endTime, _skills, team_size)
+        );
+        skillUpdate(tokenId,_skills);
+
+        employees[tokenId].projDetails.push(projectHash);
+        if (projectNumber == 1){
+            setcurrentProject(tokenId, projectNumber);
+        }
+
+        bytes32 uriHash = keccak256(abi.encode(employees[tokenId].empDetails, employees[tokenId].projDetails[projectNumber-1], employees[tokenId].skillHash)
+        );
+        string memory uri = string(abi.encodePacked(uriHash));
+        NFT.setURI(uint256(tokenId), uri);
+
+        emit NFTChanged(
+            tokenId,
+            _skills,
+            team_size,
+            _projectName,
+            startTime,
+            endTime
+        );
+    }
+
+    //Get Current Project
+    function getcurrentProject(uint8 tokenId) external view onlyOwner returns(uint8){
+        return employees[tokenId]._currentProject;
+    }
+
+    //Set Current Project
+    function setcurrentProject(uint8 tokenId,uint8 projectID) public onlyOwner{
+        employees[tokenId]._currentProject = projectID;
+    }
+
+   //Edit project
+    function editProject(
         uint8 tokenId,
         string memory skills,
         uint16 team_size,
         string memory _projectName,
         string memory startTime, //UnixTime
-        string memory endTime //UnixTime
+        string memory endTime, //UnixTime
+        uint8 projectNumber
     ) public {
         bytes32 projectHash = keccak256(
             abi.encode(_projectName, startTime, endTime, skills, team_size)
         );
-        employees[tokenId].projDetails = projectHash;
+        employees[tokenId].projDetails[projectNumber-1] = projectHash;
         bytes32 uriHash = keccak256(
-            abi.encode(employees[tokenId].empDetails, projectHash)
+            abi.encode(employees[tokenId].empDetails, employees[tokenId].projDetails[projectNumber-1], employees[tokenId].skillHash)
         );
         string memory uri = string(abi.encodePacked(uriHash));
         NFT.setURI(tokenId, uri);
@@ -107,6 +151,11 @@ import "hardhat/console.sol";
             startTime,
             endTime
         );
+    }
+
+    //Get All Projects Hash
+    function getAllProject(uint8 tokenId) public view onlyOwner returns(bytes32[] memory){
+        return employees[tokenId].projDetails;
     }
 
     //Return TokenID
@@ -123,6 +172,10 @@ import "hardhat/console.sol";
         bytes calldata
     ) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function burnProject(uint8 tokenId, uint8 projectNumber) public onlyOwner {
+        employees[tokenId].projDetails[projectNumber-1] = "";
     }
 
     //burn
